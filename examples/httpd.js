607216adb3a576e20c3181aee34823951de8518b
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-08-14 04:05:49 +0300

    Updated example

--- examples/httpd.js
+++ examples/httpd.js
@@ -1,9 +1,14 @@
 
 require('http').createServer(function (req, res) {
 	var session = require('yasession')(req, res, {'dir':'/tmp'});
+	
+	if(!session.date) session.date = new Date();
+	
 	if(!session.counter) session.counter = 0;
 	session.counter++;
+	
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\nThis is your ' + session.counter + " visit here.");
+	res.end('Hello World\nThis is your ' + session.counter + " visit here after "+ session.date + ".");
+	
 }).listen(1337, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:1337/');
